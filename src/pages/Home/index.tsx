d9943de199582a42e51ff47dbde85c8a2251a6dd
Timóteo Stifft <timoteostifft@gmail.com>
2022-07-31 17:13:23 -0300

    feat: crate home page content

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,37 @@
+import { Play } from 'phosphor-react';
 import React from 'react';
+import { CountDownContainer, FormContainer, HomeContainer, Separator } from './styles';
 
 // import { Container } from './styles';
 
 const Home: React.FC = () => {
   return (
-    <h1>Home</h1>
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="">Vou trabalhar em</label>
+          <input id='task' />
+
+          <label htmlFor="">durante</label>
+          <input type="number" id='minutesAmount' />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <button type='submit'>
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
   )
 }
 
