94935e991c3e4db8ed14bb2e05923de656fd9516
Timóteo Stifft <timoteostifft@gmail.com>
2022-08-18 11:53:16 -0300

    refactor: moved cycles reducer to separated file

---
+++ src/reducers/cycles.ts
@@ -0,0 +1,50 @@
+interface CyclesState {
+  cycles: Cycle[]
+  activeCycleId: string | null
+}
+
+export interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+  interruptedDate?: Date
+  finishedDate?: Date
+}
+
+export function cyclesReducer(state: CyclesState, action: any) {
+  switch (action.type) {
+    case 'ADD_NEW_CYCLE':
+      return {
+        ...state,
+        cycles: [...state.cycles, action.payLoad.newCycle],
+        activeCycleId: action.payLoad.newCycle.id
+      }
+    case 'INTERRUPT_CURRENT_CYCLE':
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, interruptedDate: new Date() }
+          } else {
+            return cycle
+          }
+        }),
+        activeCycleId: null
+      }
+    case 'MARK_CURRENT_CYCLE_AS_FINISHED':
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, finishedDate: new Date() }
+          } else {
+            return cycle
+          }
+        }),
+        activeCycleId: null
+      }
+    default:
+      return state
+  }
+}
